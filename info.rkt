d291cd78fd7a6a0f34f6f81f6235290aaaeec709
Jon Zeppieri <jon.zeppieri@meyouhealth.com>
2015-02-22 01:13:24 -0500

    Updates info.rkt

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define name "TZInfo")
 (define version "0.2")
 (define collection "tzinfo")
+(define scribblings '(("tzinfo.scrbl" ())))
 (define deps (list "base"
                    (list "tzdata" '#:platform 'windows)))
-
-(define scribblings '(("tzinfo.scrbl" ())))
+(define build-deps '("racket-doc" "rackunit-lib" "scribble-lib"))
