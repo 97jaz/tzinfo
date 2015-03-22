886f2ee55d0eac4d8fc53f1c9634dccda2c49e1a
Jon Zeppieri <jon.zeppieri@meyouhealth.com>
2015-03-21 20:00:56 -0400

    Fixes dependency

--- info.rkt
+++ info.rkt
@@ -5,5 +5,6 @@
 (define collection "tzinfo")
 (define scribblings '(("tzinfo.scrbl" ())))
 (define deps (list "base"
+                   "rackunit-lib"
                    (list "tzdata" '#:platform 'windows)))
-(define build-deps '("racket-doc" "rackunit-lib" "scribble-lib"))
+(define build-deps '("racket-doc" "scribble-lib"))
