bac91955633480e8e294e28094a966ab9b51fa9b
Jon Zeppieri <jon.zeppieri@meyouhealth.com>
2016-10-22 14:20:54 -0400

    Changes manner of requiring tzdata package.
    Instead of attempting to dynamic-require the tzdata package,
    we search for a variable in its package info.rkt. Based on
    that, we define runtime paths for the data files to allow
    `raco exe` to pull them in, and we modify the zoneinfo search
    path here, instead of in the tzdata package.

--- info.rkt
+++ info.rkt
@@ -1,8 +1,10 @@
 #lang info
 
 (define collection 'multi)
+(define version "0.3")
 (define deps (list "base"
                    "cldr-core"
                    "rackunit-lib"
-                   (list "tzdata" '#:platform 'windows)))
+                   (list "tzdata" '#:platform 'windows '#:version "0.3")))
+(define update-implies (list "tzdata"))
 (define build-deps '("racket-doc" "scribble-lib"))
