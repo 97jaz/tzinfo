2ab8379f8b297dc88e738ac95b9ac57d799f9a33
Jon Zeppieri <zeppieri@gmail.com>
2019-11-10 00:53:45 -0500

    Updates to work with new version of tzdata
    This fixes the problem of tzinfo not installing properly
    on Windows. Plus it allows executables to be build with
    raco exe that embed the zonefino files correctly.

--- info.rkt
+++ info.rkt
@@ -1,10 +1,10 @@
 #lang info
 
 (define collection 'multi)
-(define version "0.3")
+(define version "0.4")
 (define deps (list "base"
                    "cldr-core"
                    "rackunit-lib"
-                   (list "tzdata" '#:platform 'windows '#:version "0.3")))
+                   (list "tzdata" '#:platform 'windows '#:version "0.4")))
 (define update-implies (list "tzdata"))
 (define build-deps '("racket-doc" "scribble-lib"))
