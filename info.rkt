bb39085df49203a16ca26ce1094383cf437d520e
Jon Zeppieri <zeppieri@gmail.com>
2021-10-27 23:05:07 -0400

    Look for the tzdata package's zoneinfo files in shared directory
    The new version of tzdata copies its zoneinfo files into a shared
    directory. tzinfo will now look for them there. The advantage of
    this approach is that it should work with raco distribute.
    
    See https://github.com/97jaz/tzdata/pull/7

--- info.rkt
+++ info.rkt
@@ -1,10 +1,10 @@
 #lang info
 
 (define collection 'multi)
-(define version "0.4")
-(define deps (list "base"
+(define version "0.5")
+(define deps (list (list "base" '#:version "7.5.0.7")
                    "cldr-core"
                    "rackunit-lib"
-                   (list "tzdata" '#:platform 'windows '#:version "0.4")))
+                   (list "tzdata" '#:platform 'windows '#:version "0.5")))
 (define update-implies (list "tzdata"))
 (define build-deps '("racket-doc" "scribble-lib"))
